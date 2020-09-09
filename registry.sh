149732b40b8b02f8a769b4822c57c9e975a1f75b
kunal-bagwe <kunal@infracloud.io>
2020-09-09 16:31:24 +0530

    Changes for making cluster and deployments

--- registry.sh
+++ registry.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 # create registry container unless it already exists
-reg_name='registry-svc'
+reg_name='registry'
 reg_port='5000'
 running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
 if [ "${running}" != 'true' ]; then
